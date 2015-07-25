9b4765b80c35d56f43e286ab8cbca841d6551678
SeeLittleGirlAgain <1309230548@qq.com>
2015-07-25 12:40:20 +0800

    1、与之前的代码合并

--- udqAutoApp/platforms/android/assets/www/app/customer/home/homeCtrl.js
+++ udqAutoApp/platforms/android/assets/www/app/customer/home/homeCtrl.js
@@ -3,7 +3,18 @@ cutomer 的主页面
 功能菜单
 */
 
-angular.module('udqApp', []) /*车主的模块用cust,洗车的用user，系统公用的部分用udqApp*/
+angular.module('udqApp') /*车主的模块用cust,洗车的用user，系统公用的部分用udqApp*/
     .controller('customerHomeCtrl', ['$scope', '$state', '$ionicHistory', function ($scope, $state, $ionicHistory) {
-
+    	/*轮播图片*/
+    	$scope.images = [];
+    	/*跳转到我的订单*/
+    	$scope.goToMyOrder = function(){
+    		$ionicHistory.clearHistory();
+    		$state.go('customerMyOrder');
+    	};
+    	/*跳转到'我的点趣'*/
+    	$scope.goToMyDQ = function(){
+    		$ionicHistory.clearHistory();
+    		$state.go('customerMyDQ');
+    	};
     }])
--- udqAutoApp/platforms/android/assets/www/app/login/loginCtrl.js
+++ udqAutoApp/platforms/android/assets/www/app/login/loginCtrl.js
@@ -3,7 +3,19 @@
 管理登录页面的控制，包括customer 登录 和  employee 的登录
 根据不同的用户类型，跳转到不同的main页面
 */
-angular.module('udqApp', [])
-    .controller('loginCtrl', ['$scope', '$state', '$ionicHistory', function ($scope, $state, $ionicHistory) {
+angular.module('udqApp')
+    .controller('loginCtrl', ['$scope', '$state', '$ionicHistory', 'loginSvr', function ($scope, $state, $ionicHistory, loginSvr) {
+        
+        $scope.user = {
+            userName : '',
+            password : ''
+        };
 
+        $scope.login = function () {
+            loginSvr.loginCheck($scope.user.userName, $scope.user.password).then(function (data, msg, xhr) {
+                alert("success"+data.userType);
+            }, function (data, msg, xhr) {
+                alert("error");
+            });
+        }
     }])
