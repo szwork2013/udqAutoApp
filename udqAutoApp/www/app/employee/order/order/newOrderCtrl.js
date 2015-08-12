cc7f83c181740f3298b5eab187aecd6625e1e981
jun1943 <992717311@qq.com>
2015-08-12 11:42:22 +0800

    洗车店修改

---
+++ udqAutoApp/www/app/employee/order/order/newOrderCtrl.js
@@ -0,0 +1,40 @@
+﻿angular.module('udqApp')
+   .controller('employeeNewOrderCtrl', ['$scope', '$window', '$state', '$ionicHistory', 'employeeOrderSvr', function ($scope, $window, $state, $ionicHistory, employeeOrderSvr) {
+       $scope.orderInfo = {
+       };
+       var promise = employeeOrderSvr.getOrderByState(1);
+       promise.then(
+           function (data) {
+               $scope.orderInfo = data.rows;
+               console.log("数量：" + data.rows.length);
+           }, function (data) {
+               console.log(data);
+           }
+       );
+       /*查看某条订单信息*/
+       $scope.goToOrderInfo = function (order) {
+           $state.go('employeeOrderInfo');
+           employeeOrderSvr.saveOrderInfo(order);
+           
+       }
+       /*接收订单*/
+       $scope.acceptOrder = function (order) {
+           var promise = employeeOrderSvr.acceptOrder(order);
+           promise.then(
+               function (data) {
+                   console.log(data.msg);
+               }, function (data) {
+                   console.log(data);
+               });
+       };
+       /*取消订单*/
+       $scope.cancelOrder = function (order) {
+           var promise = employeeOrderSvr.cancelOrder(order);
+           promise.then(
+               function (data) {
+                   console.log(data.msg);
+               }, function (data) {
+                   console.log(data);
+               });
+       };
+   }])
---
+++ udqAutoApp/www/app/employee/order/order/orderListCtrl.js
@@ -0,0 +1,7 @@
+﻿angular.module('udqApp')
+	.controller('employeeOrderListCtrl', ['$scope', '$window', '$state', '$ionicHistory', function ($scope, $window, $state, $ionicHistory) {
+	    $scope.orderInfo = {
+	    };
+	   
+	  
+	}])
