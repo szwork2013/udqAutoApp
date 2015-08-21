7b5ee0f6a432a6e490f56274a5ddbb4b55c0ea5f
SeeLittleGirlAgain <1309230548@qq.com>
2015-08-21 17:37:23 +0800

    1、选择小区代码优化

--- udqAutoApp/www/app/customer/auto/autoAddCtrl.js
+++ udqAutoApp/www/app/customer/auto/autoAddCtrl.js
@@ -1,15 +1,37 @@
 angular.module('udqApp')
-	.controller('customerAutoAddCtrl', ['$scope', '$ionicPopup', '$stateParams', '$state', '$ionicHistory', '$window', 'regionSvr', 'autoSvr', function ($scope,$ionicPopup, $stateParams, $state, $ionicHistory, $window, regionSvr, autoSvr) {
-	    $scope.autoInfo = {
-	        userId: $window.localStorage['userID'],
-	        id: 0/*0为添加，1为修改*/
-	    };
-	    $scope.selected = {};
-	    $scope.selectedDistrictPid;
-	    $scope.selectedRegionPid;
+	.controller('customerAutoAddCtrl', ['$scope', '$ionicPopup', '$stateParams', '$state', '$ionicHistory', '$window', 'regionSvr', 'autoSvr', function ($scope, $ionicPopup, $stateParams, $state, $ionicHistory, $window, regionSvr, autoSvr) {
+
 	    var backName = $stateParams.backName;
 	    autoSvr.setBackName(backName);
 	    backName = autoSvr.getBackName();
+
+	    /*从service获取显示数据*/
+	    $scope.autoInfo = autoSvr.getAutoInfo();
+	    if ($scope.autoInfo == undefined) {
+	        $scope.autoInfo = {
+	            userId: $window.localStorage['userID'],
+	            id: 0/*0为添加，1为修改*/
+	        };
+	    }
+        /*获取城市、区域、小区*/
+	    regionSvr.doRequest().then(
+                    function (data) {
+                        if (data != undefined) {
+                            regionSvr.getCitiesFromData(data);
+                            $scope.cities = regionSvr.getCities();
+                            $scope.regions = regionSvr.getRegions();
+                            $scope.districts = regionSvr.getDistricts();
+                        }
+                    },
+                    function (data) {
+                        console.log(data);
+                    }
+                );
+
+	    $scope.autoInfo.selectedDistrictPid;
+	    $scope.autoInfo.selectedRegionPid;
+
+	    
 
         /*检查输入车辆信息是否合法*/
 	    var checkAutoInfo = function (auto) {
@@ -31,21 +53,6 @@ angular.module('udqApp')
 	        }
 	        return;
 	    }
-	    $scope.autoInfo.pn;
-	    regionSvr.doRequest().then(
-                    function (data) {
-                        if (data != undefined) {
-                            regionSvr.getCitiesFromData(data);
-                            $scope.cities = regionSvr.getCities();
-                            $scope.regions = regionSvr.getRegions();
-                            $scope.districts = regionSvr.getDistricts();
-                        }
-                    },
-                    function (data) {
-                        console.log(data);
-                    }
-                );
-
 		/*点击"确认"*/
 		$scope.addAuto = function () {
 		    /*1、检查车牌号、车品牌、颜色、型号、是否选择小区
@@ -74,32 +81,25 @@ angular.module('udqApp')
 
 	    };
 	    /*跳转到城市选择*/
-	    $scope.goToCitySelect = function () {
-            autoSvr.setAutoInfo($scope.autoInfo);
+		$scope.goToCitySelect = function () {
+		    saveAutoInfoToSvr($scope.autoInfo);
 	        $state.go('customerCitySelect');
 		}
 	    /*跳转到区域选择*/
 	    $scope.goToRegionSelect = function () {
-            autoSvr.setAutoInfo($scope.autoInfo);
+	        saveAutoInfoToSvr($scope.autoInfo);
 	        $state.go('customerRegion');
 		}
 	    /*跳转到小区选择*/
 	    $scope.goToDistrictSelect = function () {
-	        autoSvr.setAutoInfo($scope.autoInfo);
+	        saveAutoInfoToSvr($scope.autoInfo);
 	        $state.go('customerDistrictSelect');
-		}
+	    }
+
 		$scope.getDefaultRegionId = function (id) {
 		    $scope.autoInfo.defaultRegionId = id;
 		}
 
-	    //联动地址
-	    $scope.selectedRegionPid = autoSvr.getSelectedCityId();
-	    $scope.selectedDistrictPid = autoSvr.getSelectedRegionId();
-	    //选择的地址
-	    $scope.selected.selectedCityId = autoSvr.getSelectedCityId();
-	    $scope.selected.selectedRegionId = autoSvr.getSelectedRegionId();
-	    $scope.selected.selectedDistrictId = autoSvr.getSelectedDistrictId();
-	    /***************************************************************/
 	    /*****************************地址选择**************************/
 	    $scope.doRefreshOfCity = function () {
 	        $scope.Cities = regionSvr.getCities();
@@ -113,20 +113,20 @@ angular.module('udqApp')
 	        $scope.Districts = regionSvr.getDistricts();
 	        $scope.$broadcast('scroll.refreshComplete');
 	    }
+
 	    $scope.goBackOfCitySelect = function () {
 	        /*保存到service*/
-	        autoSvr.setSelectedCityId($scope.selected.selectedCityId);
+	        saveAutoInfoToSvr($scope.autoInfo);
 	        $state.go('customerAutoAdd', { 'typeSelect': 'city' });
 	    }
 	    $scope.goBackOfRegionSelect = function () {
 	        /*保存到service*/
-	        autoSvr.setSelectedRegionId($scope.selected.selectedRegionId);
+	        saveAutoInfoToSvr($scope.autoInfo);
 	        $state.go('customerAutoAdd', { 'typeSelect': 'region' });
 	    }
 	    $scope.goBackOfDistrictSelect = function () {
 	        /*保存到service*/
-	        autoSvr.setSelectedDistrictId($scope.selected.selectedDistrictId);
-	        autoSvr.setDefaultRegionId($scope.selected.selectedDistrictId);
+	        saveAutoInfoToSvr($scope.autoInfo);
 	        $state.go('customerAutoAdd', { 'typeSelect': 'district' });
 	    }
 		
@@ -137,8 +137,6 @@ angular.module('udqApp')
 	        } else {
 	            $state.go(backName);
 	        }
-		    
-		    
 		}
 
 		var showAlert = function (msg) {
@@ -149,6 +147,11 @@ angular.module('udqApp')
 		    alertPopup.then(function (res) {
 		        console.log(msg);
 		    });
+		}
+
+        /*保存添加车辆信息到服务中*/
+		var saveAutoInfoToSvr = function (autoInfo) {
+		    autoSvr.setAutoInfo(autoInfo);
 		}
 
 	}])
---
+++ udqAutoApp/www/app/util/takePhotoCtrl.js
@@ -0,0 +1,19 @@
+﻿angular.module('udqApp')
+    .controller('takePhotoCtrl', ['$scope', 'cameraSvr', function ($scope, cameraSvr) {
+        $scope.takPhoto = function () {
+            cameraSvr.getPicture(50,
+                function (img) {
+                    $scope.imgURI = img;
+                    console.log('seess');
+                },
+                function (data) {
+                    console.log('err');
+                });
+        }
+        $scope.ownColor = {
+            id: '121',
+            value: 'black',
+            orgId:23
+        };
+        
+    }])
