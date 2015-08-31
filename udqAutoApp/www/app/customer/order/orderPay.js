9ffbd1ef85c60b64815124824ae13f6e176af72d
SeeLittleGirlAgain <1309230548@qq.com>
2015-08-31 10:50:25 +0800

    支付

--- udqAutoApp/www/app/customer/order/orderPay.js
+++ udqAutoApp/www/app/customer/order/orderPay.js
@@ -14,14 +14,23 @@
                      //根据data内的数据判断时候成功
                      if (data.isSuccess) {
                          console.log('提交成功');
-                         pingpp.createPayment(data.data.charge, function (result) {
-                             //console.log('suc: ' + result);  //"success"
-                             alert('yes');
-                             /*从data中获取新添加的order*/
-                          customerOrderSvr.setSelectedOrder(order);
-                          $state.go('customerOrderMgr');
-                         }, function (result) {
-                             alert('err: ' + result);
+                         //pingpp.createPayment(data.data.charge, function (result) {
+                         //    //console.log('suc: ' + result);  //"success"
+                         //    alert('yes');
+                         //    /*从data中获取新添加的order*/
+                         // customerOrderSvr.setSelectedOrder(order);
+                         // $state.go('customerOrderMgr');
+                         //}, function (result) {
+                         //    alert('err: ' + result);
+                         //});
+                         pingpp.createPayment(JSON.stringify(data.data.charge), function (result, error) {
+                             if (result == "success") {
+                                 // 只有微信公众账号 wx_pub 支付成功的结果会在这里返回，其他的 wap 支付结果都是在 extra 中对应的 URL 跳转。
+                             } else if (result == "fail") {
+                                 // charge 不正确或者微信公众账号支付失败时会在此处返回
+                             } else if (result == "cancel") {
+                                 // 微信公众账号支付取消支付
+                             }
                          });
                      }
                  },
