af7eb87edca949e6631a081283e5c0d536656158
jun1943 <992717311@qq.com>
2015-09-02 10:39:43 +0800

    支付完成啦

--- udqAutoApp/www/app/customer/order/orderPay.js
+++ udqAutoApp/www/app/customer/order/orderPay.js
@@ -1,6 +1,6 @@
 ﻿angular.module('udqApp')
     .controller('customerOrderPayCtrl', ['$scope', '$stateParams', '$state', 'customerOrderMakeSvr', 'customerOrderSvr', function ($scope, $stateParams, $state, customerOrderMakeSvr, customerOrderSvr) {
-        
+
         var orderParam = angular.fromJson($stateParams.order);/*传递过来的订单信息*/
         var state = $stateParams.state;/*前一个页面的state*/
         $scope.order = orderParam;
@@ -14,13 +14,19 @@
                      //根据data内的数据判断时候成功
                      if (data.isSuccess) {
                          console.log('提交成功');
-                         pingpp.createPayment(data.data.charge, function (result) {
-                             alert('suc: ' + result);
-                             /*从data中获取新添加的order*/
-                          customerOrderSvr.setSelectedOrder(order);
-                          $state.go('customerOrderMgr');
-                         }, function (result) {
-                             alert('err: ' + result);
+                         pingpp.createPayment(data.data.charge,
+                             function (result) {
+                                 /*支付成功*/
+                                 customerOrderSvr.setSelectedOrder($scope.order);
+                                 $state.go('customerOrderMgr');
+                             },
+                         function (result) {
+                             /*fail和cancel*/
+                             if (result == 'fail') {
+                                 alert("订单支付失败");
+                             } else if (result == 'cancel') {
+                                 console.log('取消支付');
+                             }
                          });
                      }
                  },
