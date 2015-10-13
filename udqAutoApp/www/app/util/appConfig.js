0747a48bc5d8b150946b307a26a58f9fe0b3977b
jun1943 <992717311@qq.com>
2015-10-13 18:42:12 +0800

    ip

--- udqAutoApp/www/app/util/appConfig.js
+++ udqAutoApp/www/app/util/appConfig.js
@@ -13,9 +13,9 @@ angular.module('udqApp')
         后台服务配置
         */
         server: {
-            //address:'http://www.udianqu.com',
-            address: 'http://192.168.1.100',
-            name:'fzmgr',
+            address:'http://www.udianqu.com',
+            //address: 'http://192.168.1.100',
+            name:'udqweb',
             port: '8080',
             getUrl: function () {
                 return this.address + ':' + this.port + '/'+this.name+'/';
